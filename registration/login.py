7c90af41a81c1caf0f1a7477e3eb8a1a1741d4af
Seancesc <seancesc09@gmail.com>
2026-06-04 21:17:36 +0800

    Fix login import

--- registration/login.py
+++ registration/login.py
@@ -1,16 +1,18 @@
-from registration.registration import authenticate_user
+from registration.registration import authenticate_user, create_db
 
 def login_user(username, password):
     return authenticate_user(username, password)
 
-def main():
+def login():
+    create_db()
+
     username = input("Masukkan nama pengguna: ")
     password = input("Masukkan kata sandi: ")
 
     if login_user(username, password):
-        print("Login berhasil!")
+        print(f"Selamat datang, {username}!")
     else:
         print("Nama pengguna atau kata sandi salah.")
 
 if __name__ == "__main__":
-    main()
+    login()
